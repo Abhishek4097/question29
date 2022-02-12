0f8db49195a15a2b82bf0dae988519bb007c0ceb
abhishek <abhishek.sharma_cs20@gla.ac.in>
2022-02-12 20:20:35 +0530

    Initial commit

---
+++ src/EmptyTriangle.java
@@ -0,0 +1,20 @@
+public class EmptyTriangle {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = i; j < 5; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if ( i==5 || j == 1 || j == (2 * i - 1)){
+                    System.out.print("*");
+                }
+			else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
